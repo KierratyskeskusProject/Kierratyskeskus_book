475d3b4aede8921ae3d34ee4bc1683139ddae619
danni <danni.r.olsen@gmail.com>
2018-12-31 14:00:49 +0200

    message when scan

--- src/modules/Form.js
+++ src/modules/Form.js
@@ -3,24 +3,50 @@ import { Glyphicon } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import BarcodeReader from 'react-barcode-reader';
 import copy from 'clipboard-copy';
+import { ToastContainer, toast } from 'react-toastify';
 import { fetchBook } from '../redux/actions'; // fetchWeight
 import description from '../functions/description';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 class Form extends Component {
-  handleScan = (isbn) => {
+  handleScan = async (isbn) => {
     const { fetchBookData } = this.props;
     fetchBookData(isbn);
+    this.notify('Scan complete', 1);
   };
 
   handleError = err => err;
 
-
-  copyThis = async (e, data) => {
-    e.preventDefault();
+  copyThis = (e, data) => {
     let d;
     d = data.replace(/<p>/g, '');
     d = d.replace(/<\/p>/g, '\n\n');
     copy(d);
+    this.notify('Copied to clipboard', 0);
+  };
+
+  notify = (msg, operation) => {
+    switch (operation) {
+      case 0:
+        return (
+          toast.success(msg, {
+            position: toast.POSITION.TOP_RIGHT,
+          })
+        );
+      case 1:
+        return (
+          toast.info(msg, {
+            position: toast.POSITION.TOP_RIGHT,
+          })
+        );
+      default:
+        return (
+          toast.error(msg, {
+            position: toast.POSITION.TOP_RIGHT,
+          })
+        );
+    }
   };
 
   render() {
@@ -28,17 +54,34 @@ class Form extends Component {
 
     return (
       <div className="col-md-12">
+        <ToastContainer autoClose={2000} />
         <BarcodeReader onScan={this.handleScan} onError={this.handleError} />
 
-        <div className="col-md-10 whiteBox">{book.book.title}</div>
+        <div
+          className="col-md-10 whiteBox"
+          onClick={e => this.copyThis(e, book.book.title)}
+          onKeyDown={e => this.copyThis(e, book.book.title)}
+          role="button"
+          tabIndex={0}
+        >
+          {book.book.title}
+        </div>
         <div className="col-md-2 copyIcon">
           <Glyphicon
             glyph="save-file"
             onClick={e => this.copyThis(e, book.book.title)}
+            onKeyDown={e => this.copyThis(e, book.book.title)}
+            tabIndex={0}
           />
         </div>
 
-        <div className="col-md-10 whiteBox">
+        <div
+          className="col-md-10 whiteBox"
+          onClick={e => this.copyThis(e, description(book))}
+          onKeyDown={e => this.copyThis(e, description(book))}
+          role="button"
+          tabIndex={0}
+        >
           <div dangerouslySetInnerHTML={{ __html: description(book) }} />
         </div>
         <div className="col-md-2 copyIcon">
