717c29b4598ce0894faf72cab3531b59b9eff3c7
IO89 <osipov.ivan.89@gmail.com>
2019-01-22 13:32:06 +0200

    Added text recognition

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import { Form, Header, Validation } from './modules/index';
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-
         <div className="container-fluid">
           <div className="col-md-6 col-sm-6">
             <Form />
           </div>
-
           <div className="col-md-6 col-sm-6">
             <Validation />
           </div>
-
         </div>
       </div>
     );
