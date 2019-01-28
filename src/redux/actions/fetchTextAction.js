fa91df88c17d82fc0392bcafd8107c7b5d349009
IO89 <osipov.ivan.89@gmail.com>
2019-01-28 18:47:55 +0200

    Key handling

--- src/redux/actions/fetchTextAction.js
+++ src/redux/actions/fetchTextAction.js
@@ -1,36 +1,38 @@
-import { fetchTextBegin, fetchTextSuccess, fetchTextFailure } from '../types';
+import {fetchTextBegin, fetchTextSuccess, fetchTextFailure} from '../types';
+
+const GOOGLE_API_KEY = 'Key to use Google Cloud Platform';
 
 export const fetchText = (image) => {
-  const action = async (dispatch) => {
-    const data = {
-      requests: [{
-        image: { content: `${image}` },
-        features: [{ type: 'TEXT_DETECTION' }],
-      },
-      ],
+    const action = async (dispatch) => {
+        const data = {
+            requests: [{
+                image: {content: `${image}`},
+                features: [{type: 'TEXT_DETECTION'}],
+            },
+            ],
+        };
+        dispatch(fetchTextBegin());
+
+        const url = `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_API_KEY}`;
+        const request = fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        return request
+            .then(response => response.json())
+            .then((textResponse) => {
+                if (textResponse.responses[0].fullTextAnnotation) {
+                    const description = textResponse.responses[0].fullTextAnnotation.text;
+                    dispatch(fetchTextSuccess({text: description}));
+                } else {
+                    dispatch(fetchTextSuccess('no text'));
+                }
+            }, error => fetchTextFailure(error));
     };
-    dispatch(fetchTextBegin());
-
-    const url = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCR-0mjTNKnUxcHVYHMs-j6JMWkbl3BT3w';
-    const request = fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-
-    return request
-      .then(response => response.json())
-      .then((textResponse) => {
-        if (textResponse.responses[0].fullTextAnnotation) {
-          const description = textResponse.responses[0].fullTextAnnotation.text;
-          dispatch(fetchTextSuccess({ text: description }));
-        } else {
-          dispatch(fetchTextSuccess('no text'));
-        }
-      }, error => fetchTextFailure(error));
-  };
-  return action;
+    return action;
 };
